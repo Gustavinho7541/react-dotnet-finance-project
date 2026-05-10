9d1b54f5218cc6abc89af2c27769b3e042418dcf
Gustavo <gupsjales@gmail.com>
2026-05-10 17:24:13 -0300

    Createdin in  the Services at Front-End

---
+++ frontend/src/Models/User.tsx
@@ -0,0 +1,5 @@
+export type UserProfileToken = {
+    UserName: string;
+    email: string;
+    token: string;
+}
---
+++ frontend/src/Services/AuthService.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { UserProfileToken } from "../../src/Models/User";
+import { handleError } from "../../src/Helpers/ErrorHandler";
+
+const api = "http://localhost:5000/api";
+
+export const loginAPI = async (username: string, password: string) => {
+    try {
+      const data = await axios.post<UserProfileToken>(api + "account/login", {
+        username: username,
+        password: password,
+      });
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const registerAPI = async (email: string, password: string) => {
+    try {
+      const data = await axios.post<UserProfileToken>(api + "account/register", {
+        email: email,
+        password: password,
+      });
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
