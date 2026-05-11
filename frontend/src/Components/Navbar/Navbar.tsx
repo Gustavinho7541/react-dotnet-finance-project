1bc0d6b9c1e999bec16106b13571e7a5db41409b
Gustavo <gupsjales@gmail.com>
2026-05-11 00:29:11 -0300

    Modification of the NavBar and Finish at FrontEnd

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,34 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./logo.png";
+import "./Navbar.css";
+import { useAuth } from "../../Context/useAuth";
 
-const Navbar = () => {
+interface Props {}
+
+const Navbar = (props: Props) => {
+  const { isLoggedIn, user, logout } = useAuth();
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
-
-        <div className="flex items-center space-x-6">
+        <div className="flex items-center space-x-20">
           <Link to="/">
-            <img src={logo} alt="Logo" className="h-8" />
+            <img src={logo} alt="" />
           </Link>
-
-          <div className="hidden lg:flex items-center space-x-6 font-bold">
+          <div className="hidden font-bold lg:flex">
             <Link to="/search" className="text-black hover:text-darkBlue">
               Search
             </Link>
-            <a href="#" className="text-black hover:text-darkBlue">
-              Login
-            </a>
           </div>
         </div>
-
-        <a
-          href="#"
-          className="hidden lg:block px-6 py-2 font-bold rounded text-white bg-lightGreen hover:bg-darkBlue"
-        >
-          Signup
-        </a>
-
+        {isLoggedIn() ? (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <div className="hover:text-darkBlue">Welcome, {user?.userName}</div>
+            <a
+              onClick={logout}
+              className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+            >
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <Link to="/login" className="hover:text-darkBlue">
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+            >
+              Signup
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
